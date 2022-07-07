b7a35cbabb41ddc1ce8c432ccaaf18bcdd0f1356
galih diarsyah <galihdiarsyah@gmail.com>
2022-07-08 01:19:32 +0700

    create post refractoring

--- src/screens/AddNewPostDetailsScreen.tsx
+++ src/screens/AddNewPostDetailsScreen.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { RouteProp, useRoute } from '@react-navigation/native'
+import { createPostStackParamList } from '../types/navtypes'
+import * as MediaLibrary from 'expo-media-library'
+import { CameraCapturedPicture } from 'expo-camera'
+
 
 const AddNewPostDetailsScreen = () => {
+  const routeProp = useRoute<RouteProp<createPostStackParamList,"postDetails">>();
+  const {uri,...rest} =  (routeProp.params.selectedFiles as Array<MediaLibrary.Asset>)[0]; 
+  if(uri){
+    console.log(uri);
+    
+  }
   return (
     <View>
-      <Text>Add</Text>
+      {uri && <Image source={{uri : uri}} style={{width:100,height:100}}/>}
     </View>
   )
 }
